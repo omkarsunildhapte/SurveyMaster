4efd1c8a8d1057c19e458390ca5464781653c6a9
omkarsunildhapte <67897741+omkarsunildhapte@users.noreply.github.com>
2023-11-22 23:31:24 +0530

    add the auth

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-// app.js
-
 const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const dotenv = require('dotenv');
 const passport = require('passport');
+<<<<<<< Updated upstream
 const GitHubStrategy = require('passport-github').Strategy;
 const session = require('express-session');
+=======
+const GitHubStrategy = require('passport-github').Strategy; // Add GitHubStrategy
+const session = require('express-session');
+const User = require('./models/user');
+>>>>>>> Stashed changes
 
 dotenv.config();
 
@@ -17,7 +21,18 @@ app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 
+<<<<<<< Updated upstream
 // Connect to MongoDB and start the server within the connection callback
+=======
+// Set up session middleware
+app.use(session({ secret: 'b9e4d8b6a1777d83cfbffcbeab7ac69f5d04e29d2a00007c7808bb787c384a0c', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// MongoDB connection
+>>>>>>> Stashed changes
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB connected successfully.');
@@ -31,16 +46,20 @@ mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology
     process.exit(1);
   });
 
+<<<<<<< Updated upstream
 // Passport configuration
 app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+=======
+>>>>>>> Stashed changes
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
   callbackURL: 'http://localhost:3000/auth/github/callback',
 },
+<<<<<<< Updated upstream
   (accessToken, refreshToken, profile, done) => {
     // Store user information in MongoDB
     // Adjust the User model according to your needs
@@ -81,6 +100,83 @@ app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.render('home', { user: req.user });
+=======
+  function (accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+
+  }));
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+}, async (accessToken, refreshToken, profile, done) => {
+  try {
+    // Check if the user already exists in your database
+    const existingUser = await User.findOne({ googleId: profile.id });
+
+    if (existingUser) {
+      return done(null, existingUser);
+    } else {
+      // If the user doesn't exist, create a new user in the database
+      const newUser = new User({
+        googleId: profile.id,
+        username: profile.displayName,
+        // Add other user properties as needed
+      });
+
+      await newUser.save();
+      return done(null, newUser);
+    }
+  } catch (err) {
+    return done(err);
+  }
+}));
+// Serialize and deserialize user
+app.get('/login/github', passport.authenticate('github'));
+
+// GitHub callback route
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/' }),
+  (req, res) => res.redirect('/profile'));
+
+app.get('/login/google', passport.authenticate('google', { scope: ['profile'] }));
+
+// Google callback route
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => res.redirect('/profile'));
+// Profile route
+app.get('/profile', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render('profile', { user: req.user });
+  } else {
+    res.redirect('/login');
+  }
+>>>>>>> Stashed changes
 });
 
-// ...any additional routes or error handling
+// Logout route
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+// GitHub login route
+app.get('/login/github', passport.authenticate('github'));
+
+// GitHub callback route
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/' }),
+  (req, res) => res.redirect('/profile'));
+
+// Simple login route
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// Home route
+app.get('/', (req, res) => {
+  res.render('home');
+});
--- routes/surveyroutes.js
+++ routes/surveyroutes.js
@@ -1,27 +1,49 @@
 const express = require('express');
 const router = express.Router();
+<<<<<<< Updated upstream
 // const surveyController = require('./surveycontroller');
 const surveyController = require('./surveycontroller');
 // Route to display all surveys
+=======
+const passport = require('passport');
+const surveyController = require('./surveycontroller');
+const Survey = require('../models/survey'); // Adjust the path accordingly
+
+>>>>>>> Stashed changes
 router.get('/', surveyController.listSurveys);
 
-// Route to display the form for creating a new survey
 router.get('/new', surveyController.surveyCreateGet);
 
-// Route to handle the form submission for creating a new survey
 router.post('/', surveyController.surveyCreatePost);
 
-// Route to display a single survey's detail
 router.get('/:id', surveyController.surveyDetail);
 
-// Route to display the form to edit an existing survey
 router.get('/:id/edit', surveyController.surveyUpdateGet);
 
-// Route to handle the form submission for updating an existing survey
 router.put('/:id', surveyController.surveyUpdatePost);
 
+<<<<<<< Updated upstream
 // Route to handle the deletion of a survey
 router.delete('/:id', surveyController.surveyDeleteGet);
 
+=======
+router.delete('/:id', async (req, res) => {
+    // Your delete logic goes here
+});
+>>>>>>> Stashed changes
+
+// Corrected login route
+router.get('/login', (req, res) => {
+    console.log('Login route hit');
+    res.render('login');
+});
+
+router.get('/login/github', passport.authenticate('github'));
+
+router.get('/auth/github/callback',
+    passport.authenticate('github', { failureRedirect: '/' }),
+    (req, res) => {
+        res.redirect('/profile');
+    });
 
 module.exports = router;
